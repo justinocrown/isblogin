950fabe0462e840465cc418a403027c9e5db7ec4
justinocrown <72370501+justinocrown@users.noreply.github.com>
2020-11-18 12:00:20 +0700

    Add files via upload

---
+++ loginbootstrap/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 40%;
+    height: 40%;
+  }
+  body {
+  background-color: burlywood;
+}   
+     table{
+         border-spacing:20px;  
+     }
+     .div-nama{
+         background-color: #EBEBEB;
+     }
+     .div-me{
+         background-color: coral;
+     }
+     .div-3{
+         background-color: gray;
+     }
+     .div-4{
+         background-color: #333333;
+     }
+     ul{ list-style-type: none;
+         margin: 0;
+         padding: 0;
+         overflow: hidden;
+         background-color: #333333;
+     }
+     li{
+         float: left;
+     }
+     li a{
+         display:block;
+         color:white;
+         text-align-last: center;
+         padding: 16px;
+         text-decoration-line: none;
+    
+     }
+     li a :hover{
+         background-color: #111111;
+     }
+    </style>      
+
+
+
+ 
+ 
+ 
+<title>Friend List</title>
+</head>
+<body>
+         
+       
+    <div class="container"> 
+        <p class="bg-secondary text-white">
+          <h2>
+        <a href="#" title=""><img src="assests/img/Logo_0.png" width="100" height="100" class="img-responsive"></a>
+                 UKM TCG SITE </h2>
+
+                 <div class="form-group">
+						<a href= "login.php"><input type="submit" name="login" class="btn btn-primary btn-block" value="Log me in" /></a>
+					</div>
+<br> </br>
+<div class='row'>
+    <div class='col-md-4 mt-3' >
+        <a href="profilefriend.html" title=""><img src="assests/img/Cocytus.PNG" width="100%" class="img-responsive"></a>
+    </div>
+    <div class='col-md-4 mt-3'>
+        <a href="profilefriend.html" title=""><img src="assests/img/Mordred.PNG" width="100%" class="img-responsive"></a>
+    </div>
+    <div class='col-md-4  mt-3'>
+        <a href="profilefriend.html" title=""><img src="assests/img/Cocytus.PNG" width="100%" class="img-responsive"></a>
+    </div>
+    <div class='col-md-4  mt-3'>
+        <a href="profilefriend.html" title=""><img src="assests/img/Mordred.PNG" width="100%" class="img-responsive"></a>
+    </div>
+    <div class='col-md-4  mt-3'>
+        <a href="profilefriend.html" title=""><img src="assests/img/Cocytus.PNG"width="100%" class="img-responsive"></a>
+    </div>
+    <div class='col-md-4  mt-3'>
+        <a href="profilefriend.html" title=""><img src="assests/img/Mordred.PNG" width="100%" class="img-responsive"></a>
+    </div>
+    <div class='col-md-4  mt-3'>
+        <a href="profilefriend.html" title=""><img src="assests/img/Cocytus.PNG" width="100%" class="img-responsive"></a>
+    </div>
+
+</div>               
+                </table>
+
+        
+      
+
+       
+        
+      
+      
+    
+       
+</body>
+</html>
---
+++ loginbootstrap/user_mahasiswa.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+if(empty($_SESSION)){
+	header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<style>
+		body {
+			background-color:#eee;
+		}
+		.row {
+			margin:100px auto;
+			width:300px;
+			text-align:center;
+		}
+		.login {
+			background-color:#fff;
+			padding:20px;
+			margin-top:20px;
+		}
+	</style>
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	
+	<div class="container">
+		<div class="row">
+            <h1> Mahasiswa </h1>
+			<h2>Login Berhasil</h2>
+			<div class="login">
+				<p>Anda berhasil login dengan detail sebagai berikut:</p>
+				<p>Username: <?php echo $_SESSION['username']; ?><br>
+				Level: <?php echo $_SESSION['level']; ?></p>
+				<p><a href="logout.php" class="btn btn-primary" onclick="return confirm('Yakin ingin Logout?')">Log out</a></p>
+			</div>
+			
+		</div>
+	</div>
+
+	
+</body>
+</html>
